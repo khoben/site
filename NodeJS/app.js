0c6a66f8ef89d9ff3c74bcf65d3a6181d51023ad
khoben <extless@gmail.com>
2018-03-01 00:30:54 +0700

    nodejs app is done

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const twig = require('twig');
 const bodyParser = require('body-parser');
@@ -32,7 +31,7 @@ app.engine('html', twig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 format.extend(String.prototype)
@@ -56,68 +55,217 @@ const Category = carAppModel.extend({
     tableName: 'category',
 });
 
-app.get('/', function (req, res) {
+const Modification = carAppModel.extend({
+    tableName: 'modification',
+});
+
+const Charvalue = carAppModel.extend({
+    tableName: 'characteristicvalue',
+});
+
+const Charname = carAppModel.extend({
+    tableName: 'characteristicname',
+});
+
+function mysql_real_escape_string(str) {
+    if (typeof str != 'string')
+        return str;
+
+    return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
+        switch (char) {
+            case "\0":
+                return "\\0";
+            case "\x08":
+                return "\\b";
+            case "\x09":
+                return "\\t";
+            case "\x1a":
+                return "\\z";
+            case "\n":
+                return "\\n";
+            case "\r":
+                return "\\r";
+            case "\"":
+            case "'":
+            case "\\":
+            case "%":
+                return "\\" + char; // prepends a backslash to backslash, percent,
+            // and double/single quotes
+        }
+    });
+}
+
+//Done
+app.get('/', (req, res) => {
     car = new Car();
     car.find('all', (err, cars) => {
         res.render('index.twig', { cars: cars, img_folder: itemImgFolder });
     });
 });
 
-app.get('/car/:id', function (req, res) {
+//Done
+app.get('/car/:id', (req, res) => {
     car = new Car();
+    modification = new Modification();
     car.find('first', { where: "ID = {0}".format(req.params.id) }, (err, car) => {
-        res.render('item-page.twig', {
-            car: car,
-            img_folder: itemImgFolder
-        });
+        modification.query("SELECT\
+        characteristicvalue.value as value,\
+        characteristicvalue.unit as unit,\
+        characteristicname.name as name\
+        FROM modification\
+        INNER JOIN car\
+            ON modification.ID_car = car.ID\
+        INNER JOIN characteristicvalue\
+            ON characteristicvalue.ID_modification = modification.ID\
+        INNER JOIN characteristicname\
+            ON characteristicvalue.ID_char_name = characteristicname.ID\
+        WHERE car.ID = {0}".format(req.params.id), (err, modification) => {
+
+
+                res.render('item-page.twig', {
+                    car: car,
+                    img_folder: itemImgFolder,
+                    mods: modification
+                });
+            });
     });
 });
 
-app.get('/edit/car/:id', function (req, res) {
+//Done
+app.get('/edit/car/:id', (req, res) => {
     car = new Car();
     marks = new Mark();
     categories = new Category();
+    modification = new Modification();
+    characterisricName = new Charname();
     pageName = 'Редактирование';
     btnName = 'Сохранить';
     car.find('first', { where: "ID = {0}".format(req.params.id) }, (err, car) => {
         marks.find('all', (err, marks) => {
             categories.find('all', (err, categories) => {
-                res.render('edit-page.twig', {
-                    car: car,
-                    marks: marks,
-                    categories: categories,
-                    img_folder: itemImgFolder,
-                    pageName: pageName,
-                    btnName: btnName
-                });
-            })
+                modification.query("SELECT\
+        characteristicvalue.value as value,\
+        characteristicvalue.unit as unit,\
+        characteristicname.name as name\
+        FROM modification\
+        INNER JOIN car\
+            ON modification.ID_car = car.ID\
+        INNER JOIN characteristicvalue\
+            ON characteristicvalue.ID_modification = modification.ID\
+        INNER JOIN characteristicname\
+            ON characteristicvalue.ID_char_name = characteristicname.ID\
+        WHERE car.ID = {0}".format(req.params.id), (err, exist_mods) => {
+
+                        characterisricName.find('all', (err, charNames) => {
+                            res.render('edit-page.twig', {
+                                car: car,
+                                marks: marks,
+                                categories: categories,
+                                img_folder: itemImgFolder,
+                                pageName: pageName,
+                                btnName: btnName,
+                                exist_mods: exist_mods,
+                                mods: charNames
+
+                            });
+                        });
+
+                    })
+            });
         });
     });
 });
 
-app.post('/edit/car/:id', function (req, res) {
 
+app.post('/edit/car/:id', upload.single('img_path'), (req, res) => {
+
+
+    if (req.file) {
+        newImgPath = req.file.path.replace("public", "").replace("media", "");
+        console.log("UPDATE car set img_path = {0} WHERE ID = {1}".format(mysql_real_escape_string(newImgPath), req.params.id));
+
+        car = new Car();
+        car.query("UPDATE car set img_path = '{0}' WHERE ID = {1}".format(mysql_real_escape_string(newImgPath), req.params.id), (err, car) => {
+
+        });
+    }
+
+    modification = new Modification();
+    charVal = new Charvalue();
+    charName = new Charname();
+    modification.find('first', { where: "ID_car = {0}".format(req.params.id) }, (err, modification) => {
+        charVal.find('all', { where: "ID_modification = {0}".format(modification.ID) }, (err, charVal) => {
+            charName.find('all', (err, charName) => {
+
+                for (let char_id of charName) {
+
+                    var new_val = req.body[char_id.ID];
+
+
+                    if (new_val) {
+                        var update = false;
+                        for (let char_value of charVal) {
+                            if (char_value.ID_char_name == char_id.ID) {
+                                update = true;
+                                break;
+                            }
+                        }
+                        if (update) {
+
+                            updateCharVal = new Charvalue();
+                            updateCharVal.query("UPDATE characteristicvalue set value={0} WHERE ID_char_name = {1} and ID_modification = {2}".format(new_val, char_id.ID, modification.ID), (err, updateCharVal) => {
+
+                            });
+
+                        } else {
+                            newCharVal = new Charvalue({
+                                ID_modification: modification.ID,
+                                value: new_val,
+                                ID_char_name: char_id.ID
+                            });
+                            newCharVal.save();
+
+
+                        }
+                    }
+
+                }
+            });
+        });
+    });
+    res.redirect('/car/{0}'.format(req.params.id));
 });
 
-app.get('/add', function (req, res) {
+//Done
+app.get('/add', (req, res) => {
     marks = new Mark();
     categories = new Category();
+    characterisricName = new Charname();
     pageName = 'Добавление';
     btnName = 'Добавить';
 
+
     marks.find('all', (err, marks) => {
         categories.find('all', (err, categories) => {
-            res.render('edit-page.twig', {
-                marks: marks,
-                categories: categories,
-                pageName: pageName,
-                btnName: btnName
+            characterisricName.find('all', (err, charNames) => {
+                console.log(charNames);
+
+                res.render('edit-page.twig', {
+                    marks: marks,
+                    categories: categories,
+                    img_folder: itemImgFolder,
+                    pageName: pageName,
+                    btnName: btnName,
+                    mods: charNames
+
+                });
             });
-        })
+        });
     });
 });
 
-app.post('/add', upload.single('img_path'), function (req, res) {
+//Done
+app.post('/add', upload.single('img_path'), (req, res) => {
     newImgPath = req.file.path.replace("public", "").replace("media", "");
     car = new Car({
         name: req.body.name,
@@ -128,11 +276,57 @@ app.post('/add', upload.single('img_path'), function (req, res) {
     car.save((err, res) => {
         newCarID = res.insertId;
 
+        modification = new Modification({
+            name: 'Модификация для ' + req.body.name,
+            ID_car: newCarID
+        });
+
+        modification.save((err, res) => {
+            newModId = res.insertId;
+
+            characterisricName = new Charname();
+            characterisricName.find('all', (err, characterisricName) => {
+                for (let char_id of characterisricName) {
+                    new_val = req.body[char_id.ID];
+                    if (!!new_val) {
+                        charVal = new Charvalue({
+                            ID_modification: newModId,
+                            ID_char_name: char_id.ID,
+                            value: new_val
+                        });
+                        charVal.save();
+                    }
+
+                }
+            })
+
+        });
+
+
     });
 
     res.redirect('/');
 });
 
 
+//DONE
+app.get('/admin', (req, res) => {
+    car = new Car();
+    car.find('all', (err, cars) => {
+        res.render('admin-page.twig', { cars: cars, img_folder: itemImgFolder });
+    });
+});
 
-app.listen(228);
+//done
+app.get('/delete/car/:id', (req, res) => {
+    car = new Car();
+    car.set('id', req.params.id);
+    console.log(car);
+    car.remove();
+    res.redirect('/admin');
+});
+
+
+var listener = app.listen(69, '127.0.0.1', () => {
+    console.log('Started server on ' + listener.address().address + ':' + listener.address().port);
+});
