967b7b9983b056a2d70239cb4f5ff302c4eff57f
khoben <extless@gmail.com>
2018-04-01 16:20:01 +0700

    ajax: load pic

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -5,6 +5,10 @@ const mysqlModel = require('mysql-model');
 const format = require('string-format');
 const multer = require('multer');
 const path = require('path')
+const fs = require('fs')
+const formidable = require('formidable')
+const readChunk = require('read-chunk')
+const fileType = require('file-type')
 
 const itemImgFolder = '/media/'; //for twig
 const itemImgPathDB = '/item_pics/'; //for db
@@ -26,6 +30,7 @@ const upload = multer({
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use('/uploads', express.static('uploads'));
 app.engine('html', twig.renderFile);
 
 app.set('view engine', 'html');
@@ -180,15 +185,24 @@ app.get('/edit/car/:id', (req, res) => {
 app.post('/edit/car/:id', upload.single('img_path'), (req, res) => {
 
 
+    name = req.body.name;
+    ID_mark = req.body.mark;
+    ID_category = req.body.category;
+
     if (req.file) {
         newImgPath = req.file.path.replace("public", "").replace("media", "");
-        console.log("UPDATE car set img_path = {0} WHERE ID = {1}".format(mysql_real_escape_string(newImgPath), req.params.id));
 
         car = new Car();
-        car.query("UPDATE car set img_path = '{0}' WHERE ID = {1}".format(mysql_real_escape_string(newImgPath), req.params.id), (err, car) => {
+        car.query("UPDATE car set img_path = '{0}'and name = '{1}'and ID_mark = '{2}' and ID_category = '{3}' WHERE ID = {4}".format(mysql_real_escape_string(newImgPath), req.body.name, req.body.mark, req.body.category, req.params.id), (err, car) => {
+
+        });
+    } else {
+        car = new Car();
+        car.query("UPDATE car set name = '{0}'and ID_mark = '{1}' and ID_category = '{2}' WHERE ID = {3}".format(req.body.name, req.body.mark, req.body.category, req.params.id), (err, car) => {
 
         });
     }
+
 
     modification = new Modification();
     charVal = new Charvalue();
@@ -266,6 +280,7 @@ app.get('/add', (req, res) => {
 
 //Done
 app.post('/add', upload.single('img_path'), (req, res) => {
+
     newImgPath = req.file.path.replace("public", "").replace("media", "");
     car = new Car({
         name: req.body.name,
@@ -324,6 +339,72 @@ app.get('/delete/car/:id', (req, res) => {
     console.log(car);
     car.remove();
     res.redirect('/admin');
+});
+
+app.post('/upload_photos', function (req, res) {
+    var photos = [],
+        form = new formidable.IncomingForm();
+
+    // Tells formidable that there will be multiple files sent.
+    form.multiples = true;
+    // Upload directory for the images
+    form.uploadDir = path.join(__dirname, 'tmp_uploads');
+
+    // Invoked when a file has finished uploading.
+    form.on('file', function (name, file) {
+        // Allow only 3 files to be uploaded.
+        if (photos.length === 3) {
+            fs.unlink(file.path);
+            return true;
+        }
+
+        var buffer = null,
+            type = null,
+            filename = '';
+
+        // Read a chunk of the file.
+        buffer = readChunk.sync(file.path, 0, 262);
+        // Get the file type using the buffer read using read-chunk
+        type = fileType(buffer);
+
+        // Check the file type, must be either png,jpg or jpeg
+        if (type !== null && (type.ext === 'png' || type.ext === 'jpg' || type.ext === 'jpeg')) {
+            // Assign new file name
+            filename = Date.now() + '-' + file.name;
+
+            // Move the file with the new file name
+            fs.rename(file.path, path.join(__dirname, 'uploads/' + filename));
+
+            // Add to the list of photos
+            photos.push({
+                status: true,
+                filename: filename,
+                type: type.ext,
+                publicPath: 'uploads/' + filename
+            });
+        } else {
+            photos.push({
+                status: false,
+                filename: file.name,
+                message: 'Invalid file type'
+            });
+            fs.unlink(file.path);
+        }
+    });
+
+    form.on('error', function (err) {
+        console.log('Error occurred during processing - ' + err);
+    });
+
+    // Invoked when all the fields have been processed.
+    form.on('end', function () {
+        console.log('All the request fields have been processed.');
+    });
+
+    // Parse the incoming form fields.
+    form.parse(req, function (err, fields, files) {
+        res.status(200).json(photos);
+    });
 });
 
 
---
+++ NodeJS/public/static/js/load_image.js
@@ -0,0 +1,81 @@
+$('#imageInput').on('change', function () {
+    $('.progress-bar').width('0%');
+});
+
+$("#myForm button").click(function (event) {
+    event.preventDefault()// cancel form submission
+    if ($(this).attr("value") == "imgLoadBtn") {
+
+        var files = $('#imageInput').get(0).files,
+            formData = new FormData();
+
+        if (files.length === 0) {
+            alert('Изображение не выбрано.');
+            return false;
+        }
+
+        if (files.length > 1) {
+            alert('Можно выбрать только 1 изображение');
+            return false;
+        }
+
+        // Append the files to the formData.
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            formData.append('photos[]', file, file.name);
+        }
+
+        uploadFiles(formData);
+    } else {
+        $("#myForm").submit();
+    }
+});
+
+function uploadFiles(formData) {
+    $.ajax({
+        url: '/upload_photos',
+        method: 'post',
+        data: formData,
+        processData: false,
+        contentType: false,
+        xhr: function () {
+            var xhr = new XMLHttpRequest();
+
+            xhr.upload.addEventListener('progress', function (event) {
+                var progressBar = $('.progress-bar');
+
+                if (event.lengthComputable) {
+                    var percent = (event.loaded / event.total) * 100;
+                    progressBar.width(percent + '%');
+
+                    if (percent === 100) {
+                        progressBar.removeClass('active');
+                    }
+                }
+            });
+
+            return xhr;
+        }
+    }).done(handleSuccess).fail(function (xhr, status) {
+        alert(status);
+    });
+}
+
+function handleSuccess(data) {
+    if (data.length > 0) {
+        var html = '';
+        for (var i = 0; i < data.length; i++) {
+            var img = data[i];
+
+            if (img.status) {
+                html += '<img class="card-img-top" src="/' + img.publicPath + '" alt="Card image cap">';
+            } else {
+                html += '<div class="col-xs-6 col-md-4"><a href="#" class="thumbnail">Invalid file type - ' + img.filename + '</a></div>';
+            }
+        }
+
+        $('#itemImage').html(html);
+    } else {
+        alert('Ошибка. Изображение не загружено.')
+    }
+}
